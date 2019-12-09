a5618282b580d199a778529cb3bac8dd56af3c2e
nakamuloud <im.masayanakamura@gmail.com>
2019-12-09 23:49:00 +0900

    Update:二択を調整

--- src/components/TwoChoice.vue
+++ src/components/TwoChoice.vue
@@ -6,23 +6,28 @@
         <v-container>
           <v-layout>
             <v-flex>
-              <v-btn class="pa-2" fab dark large color="blue" @click="vote(true)">
-                <v-icon dark>mdi-thumb-up</v-icon>
+              <v-btn large color="error" width="150pt" @click="vote(1)">
+                {{ this.$store.state.quizValue["quizChoice1"] }}
               </v-btn>
             </v-flex>
             <v-flex>
-              <v-btn class="pa-2" fab dark large color="pink" @click="vote(false)">
-                <v-icon dark>mdi-thumb-down</v-icon>
+              <v-btn large color="primary" width="150pt" @click="vote(2)">
+                {{ this.$store.state.quizValue["quizChoice2"] }}
               </v-btn>
             </v-flex>
           </v-layout>
         </v-container>
       </v-content>
     </div>
-    <v-snackbar v-model="snackbar" :multi-line="true">
-      {{ text }}
-      <v-btn color="red" :timeout="10" text @click="snackbar = false">Close</v-btn>
-    </v-snackbar>
+    <v-dialog v-model="dialog" persistent max-width="290">
+      <v-card>
+        <v-card-title class="headline">{{ dialogTitle }}</v-card-title>
+        <v-card-text>{{ dialogText }}</v-card-text>
+        <v-card-actions>
+          <v-spacer></v-spacer>
+        </v-card-actions>
+      </v-card>
+    </v-dialog>
   </center>
 </template>
 <script>
@@ -31,23 +36,38 @@ export default {
   name: "TwoChoice",
   data: () => {
     return {
-      snackbar: false,
-      text: "投票が完了しました"
+      dialogTitle: "投票が完了しました",
+      dialogText: "再投票可能になるまで10秒ほどお待ち下さい",
+      dialog: false
     };
   },
   components: {},
   methods: {
-    vote: function(value) {
+    vote: function(ans) {
+      var that = this;
+      var updateVote = {};
+      var updateUser = {};
+      updateVote[this.$store.state.userID] = ans;
+      updateUser[this.$store.state.quizValue["quizID"]] = ans;
       firebase
         .database()
-        .ref(
-          "vote/" + this.$store.state.userID + "/" + this.$store.state.quizID
-        )
-        .set(value);
-      this.snackbar = true;
+        .ref("vote/" + this.$store.state.quizValue["quizID"] + "/")
+        .update(updateVote)
+        .then(() => {
+          firebase
+            .database()
+            .ref("user/" + this.$store.state.userID + "/")
+            .update(updateUser);
+        })
+        .then(() => {
+          this.dialog = true;
+          setTimeout(function() {
+            that.dialog = false;
+          }, 10000);
+        });
     }
   }
 };
 </script>
 
-<style/>
+<style />
